8ec6011b1cbf2fc470008d6b6e2a9cd4c458466e
Vinton Sistoza <tsistoza@gmail.com>
2024-10-05 17:43:51 -1000

    RayCasting for aiming
    Implemented RayCasting method of aiming, this will replace the already existing aim methods.

--- Assets/Scripts/AimController.cs
+++ Assets/Scripts/AimController.cs
@@ -6,6 +6,7 @@ public class AimController : MonoBehaviour
 {
     private Camera mainCam;
     private Vector3 mousePos;
+    public LayerMask groundMask;
 
     // Start is called before the first frame update
     void Start()
@@ -16,9 +17,35 @@ public class AimController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
-        Vector3 rotation = mousePos - transform.position;
-        float rotY = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0, rotY, 0);
+        Test();
+    }
+
+    private Vector3 GetMousePosition()
+    {
+        Ray ray = mainCam.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out var hitInfo, Mathf.Infinity, groundMask))
+        {
+            return hitInfo.point;
+        } else
+        {
+            return Vector3.zero;
+        }
+    }
+
+    private void Aim ()
+    {
+        Vector3 position = GetMousePosition();
+        if (position != Vector3.zero)
+        {
+            Vector3 direction = position - transform.position;
+            direction.y = -192;
+            transform.forward = direction;
+        }
+    }
+    
+    // Visualizing RayCasting
+    private void Test()
+    {
+        transform.position = GetMousePosition();
     }
 }
