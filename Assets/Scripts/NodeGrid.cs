8527fb5ae5896c976242bbe95b856dc9d58b5de2
Vinton Sistoza <tsistoza@gmail.com>
2024-10-25 21:29:05 -1000

    Grid Init
    Grid full of nodes have been made, this will help check for an enemy if a particular node or area is unwalkable.

--- Assets/Scripts/NodeGrid.cs
+++ Assets/Scripts/NodeGrid.cs
@@ -18,6 +18,7 @@ public class NodeGrid : MonoBehaviour
         nodeDiameter = nodeRadius * 2;
         gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);
         gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
+        CreateGrid();
     }
 
     // Update is called once per frame
@@ -26,14 +27,45 @@ public class NodeGrid : MonoBehaviour
         
     }
 
+    public Node NodeFromWorldPoint(Vector3 worldPos)
+    {
+        // Basically a percentage where you at 0.5 is X=0 if gridWorldSize=100 (X=-50,X=50)
+        // Similarly for the Z if it was 0.5 Z=0 if gridWorldSize=100 (Z=-50, Z=50)
+        float percentX = (worldPos.x + gridWorldSize.x/2) / gridWorldSize.x;
+        float percentZ = (worldPos.z + gridWorldSize.y/2) / gridWorldSize.y;
+        percentX = Mathf.Clamp01(percentX);
+        percentZ = Mathf.Clamp01(percentZ);
+        int x = Mathf.RoundToInt((gridSizeX - 1) * percentX);
+        int y = Mathf.RoundToInt((gridSizeY - 1) * percentZ);
+        return grid[x, y];
+    }
+
     void CreateGrid()
     {
         grid = new Node[gridSizeX, gridSizeY];
-        Vector3 worldBottomLeft = transform.position;
+        Vector3 worldBottomLeft = transform.position - (Vector3.right * gridWorldSize.x / 2) - Vector3.forward * gridWorldSize.y / 2;
+
+        for (int x = 0; x < gridSizeX; x++)
+        {
+            for (int y = 0; y < gridSizeY; y++)
+            {
+                Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius);
+                bool walkable = !(Physics.CheckSphere(worldPoint, nodeRadius, unwalkableMask));
+                grid[x, y] = new Node(walkable, worldPoint);
+            }
+        }
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, 1, gridWorldSize.y));
+        if (grid != null)
+        {
+            foreach (Node node in grid)
+            {
+                Gizmos.color = (node.walkable) ? Color.white : Color.red;
+                Gizmos.DrawCube(node.worldPos, Vector3.one * (nodeDiameter-.1f));
+            }
+        }
     }
 }
